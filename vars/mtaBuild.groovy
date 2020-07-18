b8b2af5e1bf675f68abf6dcba2408f575326a0aa
ravishankarojha <ravi.oj@hcl.com>
2020-07-18 22:56:17 +0530

    Update mtaBuild.groovy

--- vars/mtaBuild.groovy
+++ vars/mtaBuild.groovy
@@ -98,8 +98,8 @@ void call(Map parameters = [:]) {
             echo "[INFO] Executing mta build call: '${mtaCall}'."
 
             sh """#!/bin/bash
-            export HOME=/hana/backup
-            export PATH=./node_modules/.bin:${PATH}
+            //export HOME=/hana/backup
+            //export PATH=./node_modules/.bin:${PATH}
             $mtaCall
             """
 
